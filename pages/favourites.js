149fc9d6a2dfccbf911e21d580a62bb0bd987ee4
hvlimbachiya <harshlimbachiya98@gmail.com>
2022-12-06 17:53:42 -0500

    final

---
+++ pages/favourites.js
@@ -0,0 +1,35 @@
+import React from 'react'
+import { useAtom } from 'jotai'
+import { favouritesAtom } from '../store'
+import {Row, Col } from 'react-bootstrap'
+import ArtworkCard from '../components/ArtworkCard'
+import { Card } from 'react-bootstrap'
+
+export default function Favourites() {
+  const [favouritesList, setFavouritesList] = useAtom(favouritesAtom)
+
+  if (!favouritesList) return null
+
+  return (
+      <Row className='gy-4'>
+          {!favouritesList.length ? (
+              <Card>
+                  <Card.Body>
+                      <Card.Title>
+                          <h4>Nothing Here</h4>
+                      </Card.Title>
+                      <Card.Text>
+                          Try adding some new artwork to the list.
+                      </Card.Text>
+                  </Card.Body>
+              </Card>
+          ) : (
+              favouritesList.map((objectID) => (
+                  <Col lg={3} key={objectID}>
+                      <ArtworkCard objectID={objectID} />
+                  </Col>
+              ))
+          )}
+      </Row>
+  )
+}
